760643e170c7803a6f9ea61a2969ae36525d3392
Kayk Mascarenhas <ksm.kayk@outlook.com>
2020-02-10 18:53:11 -0300

    mais css

--- app/src/components/messenger.js
+++ app/src/components/messenger.js
@@ -32,10 +32,47 @@ export default class messenger extends Component {
 
     return (
       <div style={style} className="appMessenger">
-        <div className="header">Header</div>
+        <div className="header">
+          <div className="left">
+            <div className="actions">
+              <button>New message</button>
+            </div>
+          </div>
+          <div className="content">
+            <h2>Tittle</h2>
+          </div>
+          <div className="right">
+            <div className="userBar">
+              <div className="profileName">Emanulle Leticia</div>
+              <div className="profileImage">
+                <img
+                  src="https://gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="main">
           <div className="leftSidebar">Left Sidebar</div>
-          <div className="content">Content</div>
+          <div className="content">
+            <div className="messages">
+              <div className="message">
+                <div className="messageUserImage">
+                  <img
+                    src="https://gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y"
+                    alt=""
+                  />
+                </div>
+                <div className="messageBody">
+                  <div className="messageAuthor">Emanu says:</div>
+                  <div className="messageText">
+                    <p>Hello there..</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="rightSidebar">Right Sidebar</div>
         </div>
       </div>
